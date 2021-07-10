cf224622c559124e55a07e8b74cf383fa077ef8c
JoaoCioffi <joaorcioffi@gmail.com>
2021-07-10 20:20:46 -0300

    Commit 02: 'Calculator.py' (V1) Modified, (V2) Created

--- Calculator.py
+++ Calculator.py
@@ -10,12 +10,22 @@ def btnClick(num): #-> gets the pressed key
     operator = operator + str(num)
     text_Input.set(operator)
 
+def btnClearDisplay():
+    global operator
+    operator = ''
+    text_Input.set('')
+
+def btnEqualsInput():
+    global operator
+    sumup = str(eval(operator))
+    text_Input.set(sumup)
+    operator=''
 
 
 #%% Main Objects
 cal = Tk()
 cal.title('Calculator')
-operator = " "
+operator = ''
 text_Input = StringVar()
 
 #%% Display Objects:
@@ -28,39 +38,36 @@ txtDisplay = Entry(cal,font=('arial', 12, 'bold'),
 # Keys:
 
 keys_general = list(range(0,10))
-
-j = [[],[],[]]
 for i in range(0,4):   #-> keys 7,8,9 and '+' operator
     while i<3:
-        j[i].append(i+7)
         key = Button(cal,padx=16,pady=16,bd=8,fg='black',font=('arial', 12, 'bold'),
-                     text=str(i+7),bg='gray',command=lambda:btnClick(j[i])).grid(row=1,column=i)
+                     text=str(i+7),bg='gray',command = lambda i=i+7:btnClick(i)).grid(row=1,column=i)
         break
     if i == 3:
         key = Button(cal,padx=16,pady=16,bd=8,fg='black',font=('arial', 12, 'bold'),
-                     text='+',bg='gray').grid(row=1,column=i)
+                     text='+',bg='gray',command = lambda:btnClick('+')).grid(row=1,column=i)
 del i
 
 
 for i in range(0,4):   #-> keys 4,5,6 and '-' operator
     while i<3:
         key = Button(cal,padx=16,pady=16,bd=8,fg='black',font=('arial', 12, 'bold'),
-                     text=str(i+4),bg='gray').grid(row=2,column=i)
+                     text=str(i+4),bg='gray',command = lambda i=i+4:btnClick(i)).grid(row=2,column=i)
         break
     if i == 3:
         key = Button(cal,padx=16,pady=16,bd=8,fg='black',font=('arial', 12, 'bold'),
-                     text='-',bg='gray').grid(row=2,column=i)
+                     text='-',bg='gray',command = lambda:btnClick('-')).grid(row=2,column=i)
 del i
 
 
 for i in range(0,4):   #-> keys 1,2,3 and '*' operator
     while i<3:
         key = Button(cal,padx=16,pady=16,bd=8,fg='black',font=('arial', 12, 'bold'),
-                     text=str(i+1),bg='gray').grid(row=3,column=i)
+                     text=str(i+1),bg='gray',command = lambda i=i+1:btnClick(i)).grid(row=3,column=i)
         break
     if i == 3:
         key = Button(cal,padx=16,pady=16,bd=8,fg='black',font=('arial', 12, 'bold'),
-                     text='*',bg='gray').grid(row=3,column=i)
+                     text='*',bg='gray',command = lambda:btnClick('*')).grid(row=3,column=i)
 del i
 
 
@@ -68,11 +75,17 @@ keys = ['0','C','=','/']
 for i in range(0,4):   #-> keys 0,C and '/','=' operators
     while i<3:
         key = Button(cal,padx=16,pady=16,bd=8,fg='black',font=('arial', 12, 'bold'),
-                     text=keys[i],bg='gray').grid(row=4,column=i)
+                     text=keys[i],bg='gray',command = lambda i=i:btnClick(keys[i])).grid(row=4,column=i)
+        if i == 1:
+            key = Button(cal,padx=16,pady=16,bd=8,fg='black',font=('arial', 12, 'bold'),
+                     text=keys[i],bg='gray',command = btnClearDisplay).grid(row=4,column=i)
+        elif i == 2:
+            key = Button(cal,padx=16,pady=16,bd=8,fg='black',font=('arial', 12, 'bold'),
+                     text=keys[i],bg='gray',command = btnEqualsInput).grid(row=4,column=i)
         break
     if i == 3:
         key = Button(cal,padx=16,pady=16,bd=8,fg='black',font=('arial', 12, 'bold'),
-                     text=keys[i],bg='gray').grid(row=4,column=i)
+                     text=keys[i],bg='gray',command = lambda:btnClick('/')).grid(row=4,column=i)
 del i
 
 #%% Loop
